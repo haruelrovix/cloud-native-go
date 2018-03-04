a6b02e58f68fbcccb014c8ba825396241cbad14d
haruelrovix <havitchoirul.rovik@mitrais.com>
2018-03-04 09:49:29 +0700

    Change implementation of Get All Books

--- api/book.go
+++ api/book.go
@@ -8,9 +8,24 @@ import (
 // Book type with Name, Author and ISBN
 type Book struct {
 	// define the book
-	Title  string `json:"title"`
-	Author string `json:"author"`
-	ISBN   string `json:"isbn"`
+	Title       string `json:"title"`
+	Author      string `json:"author"`
+	ISBN        string `json:"isbn"`
+	Description string `json:"description,omitempty"`
+}
+
+var books = map[string]Book{
+	"1569319200": Book{
+		Title:       "Dragon Ball",
+		Author:      "Akira Toriyama",
+		ISBN:        "1569319200",
+		Description: "a Japanese media franchise created in 1984",
+	},
+	"1569319049": Book{
+		Title:  "Yu Yu Hakusho",
+		Author: "Yoshihiro Togashi",
+		ISBN:   "1569319049",
+	},
 }
 
 // ToJSON to be used for marshalling of Book type
@@ -34,14 +49,30 @@ func FromJSON(data []byte) Book {
 	return book
 }
 
-var Books = []Book{
-	Book{Title: "Dragon Ball", Author: "Akira Toriyama", ISBN: "1569319200"},
-	Book{Title: "Yu Yu Hakusho", Author: "Yoshihiro Togashi", ISBN: "1569319049"},
+// AllBooks returns a slice of all books
+func AllBooks() []Book {
+	v := make([]Book, 0, len(books))
+	for _, value := range books {
+		v = append(v, value)
+	}
+
+	return v
 }
 
 // BooksHandleFunc to be used as http.HandleFunc for Book API
 func BooksHandleFunc(w http.ResponseWriter, r *http.Request) {
-	b, err := json.Marshal(Books)
+	switch method := r.Method; method {
+	case http.MethodGet:
+		books := AllBooks()
+		writeJSON(w, books)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unsupported request method."))
+	}
+}
+
+func writeJSON(w http.ResponseWriter, books []Book) {
+	b, err := json.Marshal(books)
 	if err != nil {
 		panic(err)
 	}
