4ee525152a2f1b2ea872aabd441b4f5fd1551074
haruelrovix <havitchoirul.rovik@mitrais.com>
2018-03-04 08:38:12 +0700

    Implement TestBookFromJSON and unit test

--- api/book_test.go
+++ api/book_test.go
@@ -7,13 +7,17 @@ import (
 )
 
 func TestBookToJSON(t *testing.T) {
-	book := Book{Title: "Cloud Native Go.", Author: "M.-L. Reimer", ISBN: "0123456789"}
+	book := Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"}
 	json := book.ToJSON()
 
-	assert.Equal(t, `{"Title":"Cloud Native Go.","Author":"M.-L. Reimer","ISBN":"0123456789"}`,
+	assert.Equal(t, `{"Title":"Cloud Native Go","Author":"M.-L. Reimer","ISBN":"0123456789"}`,
 		string(json), "Book JSON marshalling wrong.")
 }
 
 func TestBookFromJSON(t *testing.T) {
-	assert.True(t, true, "Implement me.")
+	json := []byte(`{"Title":"Cloud Native Go","Author":"M.-L. Reimer","ISBN":"0123456789"}`)
+	book := FromJSON(json)
+
+	assert.Equal(t, Book{Title: "Cloud Native Go", Author: "M.-L. Reimer", ISBN: "0123456789"},
+		book, "Book JSON unmarshalling wrong.")
 }
